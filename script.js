04d6c68fe48228a26beb4548de5d918918301550
Artem Logvinov <artemartem330@yandex.ru>
2024-08-30 18:25:43 +0300

    popup

--- script.js
+++ script.js
@@ -2,8 +2,18 @@ const bg = document.getElementById("bg");
 const homeBtn = document.querySelector(".home");
 const mainSlider = document.querySelector(".main-slider");
 const nextBtn = document.querySelector(".next");
+const nextPug = document.querySelector(".btn-next");
+const prevPug = document.querySelector(".btn-prev");
+const text1 = document.querySelector(".text1");
+const text2 = document.querySelector(".text2");
+const circlePug1 = document.querySelector(".circle1");
+const circlePug2 = document.querySelector(".circle2");
+const moreBtn = document.querySelector(".more");
+const popup = document.querySelector(".popup");
+const cross = document.querySelector(".cross");
 var start = null;
 var end = null;
+var popupOpened = false;
 
 window.addEventListener("mousedown", (event) => {
   event.preventDefault();
@@ -12,9 +22,14 @@ window.addEventListener("mousedown", (event) => {
 });
 window.addEventListener("mouseup", (event) => {
   end = event.clientX;
-  if (start < end) {
+  if (end - start >= 20) {
     rightSwipe();
-  } else if (start !== null && end !== null && start != end && start > end) {
+  } else if (
+    start !== null &&
+    end !== null &&
+    start != end &&
+    start - end >= 20
+  ) {
     leftSwipe();
   }
   start = null;
@@ -27,8 +42,40 @@ nextBtn.addEventListener("click", () => {
   bg.style.left = -1024 + "px";
   mainSlider.style.left = -1024 + "px";
 });
+moreBtn.addEventListener("click", () => {
+  if (!popupOpened) {
+    popup.style.visibility = "visible";
+    popup.style.opacity = 1;
+    popupOpened = true;
+  }
+});
+cross.addEventListener("click", () => {
+  popup.style.visibility = "hidden";
+  popup.style.opacity = 0;
+  popupOpened = false;
+});
+nextPug.addEventListener("click", () => {
+  text2.style.visibility = "visible";
+  text1.style.visibility = "hidden";
+  text2.style.opacity = 1;
+  text1.style.opacity = 0;
+  circlePug1.classList.add("circle2");
+  circlePug1.classList.remove("circle1");
+  circlePug2.classList.add("circle1");
+  circlePug2.classList.remove("circle2");
+});
+prevPug.addEventListener("click", () => {
+  text1.style.visibility = "visible";
+  text2.style.visibility = "hidden";
+  text1.style.opacity = 1;
+  text2.style.opacity = 0;
+  circlePug1.classList.add("circle1");
+  circlePug1.classList.remove("circle2");
+  circlePug2.classList.add("circle2");
+  circlePug2.classList.remove("circle1");
+});
 // window.addEventListener("touchstart", (event) => {
-//   end = event.touches[0].clientX;
+//   start = event.touches[0].clientX;
 //   end = null;
 // });
 // window.addEventListener("touchend", (event) => {
