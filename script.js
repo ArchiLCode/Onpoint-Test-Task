01f965e0d2fc4746bff8ce36afcb0c3d82af4900
Artem Logvinov <artemartem330@yandex.ru>
2024-08-31 00:15:13 +0300

    custom scroll

--- script.js
+++ script.js
@@ -11,11 +11,16 @@ const circlePug2 = document.querySelector(".circle2");
 const moreBtn = document.querySelector(".more");
 const popup = document.querySelector(".popup");
 const cross = document.querySelector(".cross");
+const spermArr = document.querySelectorAll(".sperm");
+const scrollThumb = document.querySelector(".scroll-thumb");
+const scrollTrack = document.querySelector(".scroll-track");
+const scrollableText = document.querySelector(".scrollable-text");
 var start = null;
 var end = null;
 var popupOpened = false;
 var touches = [];
 
+//Desctop swipes
 window.addEventListener("mousedown", (event) => {
   event.preventDefault();
   start = event.clientX;
@@ -35,14 +40,45 @@ window.addEventListener("mouseup", (event) => {
   }
   start = null;
 });
+
+//Mobile swipes
+window.addEventListener("touchstart", (event) => {
+  start = event.touches[0].clientX;
+  end = null;
+  touches = [];
+});
+window.addEventListener("touchmove", (event) => {
+  touches.push(event.touches[event.touches.length - 1]);
+});
+window.addEventListener("touchend", (event) => {
+  end = touches[touches.length - 1].clientX;
+
+  if (end - start >= 20) {
+    rightSwipe();
+  } else if (
+    start !== null &&
+    end !== null &&
+    start != end &&
+    start - end >= 20
+  ) {
+    leftSwipe();
+  }
+  start = null;
+});
+
+//Redirect to first slide
 homeBtn.addEventListener("click", () => {
   bg.style.left = 0 + "px";
   mainSlider.style.left = 0 + "px";
 });
+
+//Redirect to second slide
 nextBtn.addEventListener("click", () => {
   bg.style.left = -1024 + "px";
   mainSlider.style.left = -1024 + "px";
 });
+
+//Show popup
 moreBtn.addEventListener("click", () => {
   if (!popupOpened) {
     popup.style.visibility = "visible";
@@ -50,11 +86,15 @@ moreBtn.addEventListener("click", () => {
     popupOpened = true;
   }
 });
+
+//Close popup
 cross.addEventListener("click", () => {
   popup.style.visibility = "hidden";
   popup.style.opacity = 0;
   popupOpened = false;
 });
+
+//Pagination buttons
 nextPug.addEventListener("click", () => {
   text2.style.visibility = "visible";
   text1.style.visibility = "hidden";
@@ -76,44 +116,71 @@ prevPug.addEventListener("click", () => {
   circlePug2.classList.remove("circle1");
 });
 
-window.addEventListener("touchstart", (event) => {
-  start = event.touches[0].clientX;
-  end = null;
-  touches = [];
-});
-
-window.addEventListener("touchmove", (event) => {
-  touches.push(event.touches[event.touches.length - 1]);
-});
-
-window.addEventListener("touchend", (event) => {
-  end = touches[touches.length - 1].clientX;
-
-  if (end - start >= 20) {
-    rightSwipe();
-  } else if (
-    start !== null &&
-    end !== null &&
-    start != end &&
-    start - end >= 20
-  ) {
-    leftSwipe();
-  }
-  start = null;
-});
-
+//Swipe functions
 const rightSwipe = () => {
   if (bg.style.left.slice(0, -2) < 0) {
     bg.style.left = Number(bg.style.left.slice(0, -2)) + Number(1024) + "px";
     mainSlider.style.left =
       Number(mainSlider.style.left.slice(0, -2)) + Number(1024) + "px";
   }
+  if (bg.style.left == "-1024px") {
+    spermArr.forEach((el) => {
+      el.classList.add("active");
+    });
+  } else {
+    spermArr.forEach((el) => {
+      el.classList.remove("active");
+    });
+  }
 };
-
 const leftSwipe = () => {
   if (bg.style.left.slice(0, -2) > -2048) {
     bg.style.left = Number(bg.style.left.slice(0, -2)) - Number(1024) + "px";
     mainSlider.style.left =
       Number(mainSlider.style.left.slice(0, -2)) - Number(1024) + "px";
   }
+  if (bg.style.left == "-1024px") {
+    spermArr.forEach((el) => {
+      el.classList.add("active");
+    });
+  } else {
+    spermArr.forEach((el) => {
+      el.classList.remove("active");
+    });
+  }
 };
+
+//Custom scroll
+let drag = false;
+scrollThumb.addEventListener("mousedown", () => {
+  drag = true;
+});
+scrollThumb.addEventListener("mousemove", (event) => {
+  if (event.clientY >= 240 && event.clientY <= 586 && drag) {
+    scrollThumb.style.top = event.clientY - 240 + "px";
+    scrollableText.style.transform = `translateY(${240 - event.clientY}px)`;
+  }
+});
+scrollThumb.addEventListener("mouseup", () => {
+  drag = false;
+});
+
+scrollThumb.addEventListener("touchstart", () => {
+  drag = true;
+});
+scrollThumb.addEventListener("touchmove", (event) => {
+  if (
+    event.touches[event.touches.length - 1].clientY >= 240 &&
+    event.touches[event.touches.length - 1].clientY <= 586 &&
+    drag
+  ) {
+    scrollThumb.style.top =
+      event.touches[event.touches.length - 1].clientY - 240 + "px";
+    scrollableText.style.transform = `translateY(${
+      240 - event.touches[event.touches.length - 1].clientY
+    }px)`;
+  }
+});
+scrollThumb.addEventListener("touchend", () => {
+  drag = false;
+});
